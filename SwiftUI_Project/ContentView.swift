63ca696ebfa72db21807a06d6ffe3dae44c00a49
LeeHoJun <ghwns23@gmail.com>
2020-05-03 23:17:22 +0900

    Initial Commit

---
+++ SwiftUI_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Project
+//
+//  Created by LEE on 2020/05/03.
+//  Copyright © 2020 LEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
